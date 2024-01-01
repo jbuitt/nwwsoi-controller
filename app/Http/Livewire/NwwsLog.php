4905b6f1bebc0271ca1e115a798497300d940752
Jim Buitt <jbuitt@gmail.com>
2024-01-01 08:02:44 -0600

    Added nwws log Livewire component

---
+++ app/Http/Livewire/NwwsLog.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class NwwsLog extends Component
+{
+    public $lastTenLogLines = 'Loading...<br /><br /><br /><br /><br /><br /><br /><br /><br />';
+
+    public function getLogLines()
+    {
+        $logFile = base_path() . '/storage/logs/nwws-' . date('Y-m-d') . '.log';
+        if (file_exists($logFile)) {
+            $this->lastTenLogLines = shell_exec('tail ' . $logFile);
+        }
+        $this->lastTenLogLines = 'The file ' . $logFile . ' was not found.<br /><br /><br /><br /><br /><br /><br /><br /><br />';
+    }
+
+    public function render()
+    {
+        return view('livewire.nwws-log');
+    }
+}
---
+++ resources/views/livewire/nwws-log.blade.php
@@ -0,0 +1,3 @@
+<div wire:poll="getLogLines">
+<pre>{!! $lastTenLogLines !!}</pre>
+</div>
