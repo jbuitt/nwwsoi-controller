5cb29fbc0402aa52c9058d81b1a7df7bfcd7a026
Jim Buitt <jbuitt@gmail.com>
2024-05-31 14:21:08 -0500

    Disabling running npm run build during deploy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -107,8 +107,8 @@
         echo 'NWWS-OI Controller - Running database migrations..'
         docker exec {{ $releases[$i-1] }}-nwwsoi_controller-1 ./artisan migrate --seed --force --isolated
 
-        echo 'NWWS-OI Controller - Running npm run build (again)..'
-        docker exec {{ $releases[$i-1] }}-nwwsoi_controller-1 su - sail -c "cd /var/www/html/ && npm run build"
+        # echo 'NWWS-OI Controller - Running npm run build (again)..'
+        # docker exec {{ $releases[$i-1] }}-nwwsoi_controller-1 su - sail -c "cd /var/www/html/ && npm run build"
     @endfor
 @endtask
 
