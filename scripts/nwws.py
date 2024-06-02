a19339d40f0bf1ea18479bb12e3146c98a3ae192
Jim Buitt <jbuitt@gmail.com>
2024-06-01 19:16:21 -0500

    Removing spaces from AWIPS ID when naming products

--- scripts/nwws.py
+++ scripts/nwws.py
@@ -217,10 +217,7 @@ class MUCBot(slixmpp.ClientXMPP):
             itemlist = xmldoc.getElementsByTagName('x')
             ttaaii = itemlist[0].attributes['ttaaii'].value.lower()
             cccc = itemlist[0].attributes['cccc'].value.lower()
-            awipsid = itemlist[0].attributes['awipsid'].value.lower()
-            if not re.match("^[a-z0-9]{6}$", awipsid):
-                logging.info("awipsid does match regex, skipping")
-                return
+            awipsid = itemlist[0].attributes['awipsid'].value.lower().replace(' ', '')
             id = itemlist[0].attributes['id'].value.replace('nwws_processor', '0000')
             # id = itemlist[0].attributes['id'].value
             content = itemlist[0].firstChild.nodeValue
