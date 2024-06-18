6be1611124281e7fd14429d1c03eb20a8eb7872c
Jim Buitt <jbuitt@gmail.com>
2024-06-18 09:51:35 -0500

    Fixing docker compose build issue

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -88,7 +88,7 @@
         export COMPOSE_PROJECT_NAME={{ $releases[$i-1] }}
 
         echo 'NWWS-OI Controller - (Re)building Docker images..'
-        docker compose -p {{ $release }} --env-file docker.env build
+        docker compose --env-file docker.env build
 
         echo 'NWWS-OI Controller - Starting new Docker containers..'
         docker compose --env-file docker.env up -d
